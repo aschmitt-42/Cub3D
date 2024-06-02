cee85119a99d2023c440e994aa2d6a0e3f6fda47
Zelsher <eboumaza.trav@gmail.com>
2024-06-02 18:55:41 +0200

    a

--- srcs/render_frame.c
+++ srcs/render_frame.c
@@ -6,7 +6,7 @@
 /*   By: eboumaza <eboumaza.trav@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 17:25:32 by eboumaza          #+#    #+#             */
-/*   Updated: 2024/06/02 18:05:58 by eboumaza         ###   ########.fr       */
+/*   Updated: 2024/06/02 18:55:28 by eboumaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ int	render_next_frame(t_game *game)
 {
 	if (!game->mlibx.mlx_ptr || !game->mlibx.win_ptr || !game->SO)
 		printf("test\n");
-	//mlx_put_image_to_window (game->mlibx.mlx_ptr, game->mlibx.win_ptr, game->SO, 1, 1);
+	//mlx_put_image_to_window (game->mlibx.mlx_ptr, game->mlibx.win_ptr, game->NO, 300, 300);
 	return (0);
 }
